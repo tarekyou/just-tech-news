1a7bfc4cd80decade83fd9a6cb2f0ca425004b3a
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-12-04 18:25:19 -0800

    single post page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -33,8 +33,12 @@ router.get('/', (req, res) => {
         // console.log(dbPostData[0]);
         const posts = dbPostData.map(post => post.get({ plain: true }));
         // res.render('homepage', dbPostData[0].get({ plain: true }));
-        res.render('homepage', { posts });
-      })
+        // res.render('homepage', { posts });
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+          });
+        })
       .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -50,6 +54,73 @@ router.get('/login', (req, res) => {
 
     res.render('login');
 });
+
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+  
+        // pass data to template
+        // res.render('single-post', { post });
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+          });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+
+// router.get('/post/:id', (req, res) => {
+//     const post = {
+//       id: 1,
+//       post_url: 'https://handlebarsjs.com/guide/',
+//       title: 'Handlebars Docs',
+//       created_at: new Date(),
+//       vote_count: 10,
+//       comments: [{}, {}],
+//       user: {
+//         username: 'test_user'
+//       }
+//     };
+  
+//     res.render('single-post', { post });
+// });
 
 // router.get('/', (req, res) => {
 //   res.render('homepage', {
