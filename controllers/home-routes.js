0596542149808d81aa0dca57f025231996ea3d6e
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-12-04 16:36:16 -0800

    login and logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,7 @@ const { Post, User, Comment } = require('../models');
 
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
       attributes: [
         'id',
@@ -38,7 +39,17 @@ router.get('/', (req, res) => {
         console.log(err);
         res.status(500).json(err);
       });
-  });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+
+    res.render('login');
+});
 
 // router.get('/', (req, res) => {
 //   res.render('homepage', {
